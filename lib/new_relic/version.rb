2c2bcb1e9a86b212cb7d7b6b02274f49f6a8b428
kayser <kayser@596fc9dc-a48c-484e-8c9c-6b9a9780cddb>
2009-03-17 20:14:36 +0000

    Indicate expired repository with version 999
    git-svn-id: http://svn.newrelic.com/rpm/agent/newrelic_rpm@3988 596fc9dc-a48c-484e-8c9c-6b9a9780cddb

--- lib/new_relic/version.rb
+++ lib/new_relic/version.rb
@@ -3,7 +3,7 @@ module NewRelic
   module VERSION #:nodoc:
   MAJOR = 2
   MINOR = 8
-  TINY  = 5
+  TINY  = 999
   STRING = [MAJOR, MINOR, TINY].join('.')
   def self.changes
     puts "NewRelic RPM Plugin Version: #{NewRelic::VERSION::STRING}"
